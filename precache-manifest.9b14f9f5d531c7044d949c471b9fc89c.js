7c8f33aac67ad3b0c592457e772a342f6e0d8891
jbhammett <jbhammett@users.noreply.github.com>
2023-12-21 18:39:21 +0000

    deploy: 9198a2fca1329245918c5ea78910dee2dfe7ca8b

---
+++ precache-manifest.9b14f9f5d531c7044d949c471b9fc89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88791d24bacb43233414b66f8f95f23",
+    "url": "/assignment-3-jbhammett/index.html"
+  },
+  {
+    "revision": "911444808c3dd98319fc",
+    "url": "/assignment-3-jbhammett/static/css/main.02971024.chunk.css"
+  },
+  {
+    "revision": "2e91ac9a7061d87a046d",
+    "url": "/assignment-3-jbhammett/static/js/2.42b4dc57.chunk.js"
+  },
+  {
+    "revision": "176fdc199986cc7cdf73c516848cd310",
+    "url": "/assignment-3-jbhammett/static/js/2.42b4dc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911444808c3dd98319fc",
+    "url": "/assignment-3-jbhammett/static/js/main.28fd1b06.chunk.js"
+  },
+  {
+    "revision": "943f3b654759d7966ed2",
+    "url": "/assignment-3-jbhammett/static/js/runtime-main.e8516c0c.js"
+  }
+]);
